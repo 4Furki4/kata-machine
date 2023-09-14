410700b754e75405ec625ae666dc36c0cc814de5
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-14 20:33:16 +0300

    add: Binary Search pseudo code and solution

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,35 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // take the starting and ending points
 
+    // find the middlepoint
+
+    // compare if the value on the middle is eql to needle
+
+    // if so, return true
+
+    // if not, check if the middle value is greater than needle
+
+    // if so, assign middle point to heighest point
+
+    // if not, assign middle point to heighest point + 1
+
+    // return step 3 unless heighest point gets lower than lower point.
+
+    let lowestVal = 0,
+        heightestVal = haystack.length;
+    do {
+        const middleValue = Math.floor(
+            lowestVal + (heightestVal - lowestVal) / 2,
+        );
+        const value = haystack[middleValue];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            heightestVal = middleValue;
+        } else {
+            lowestVal = middleValue + 1;
+        }
+    } while (lowestVal < heightestVal);
+
+    return false;
 }
