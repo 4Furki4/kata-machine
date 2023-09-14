d073c3661640a64a19f7ea2e07ec06e61a8ff669
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-14 21:10:58 +0300

    add: TwoCrystallBall problem psuedo code and solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,30 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // given two crystal balls that will break if dropped from high enough distance, determine the exact spot in which it will break in the most optimized way.
+    // assign jmpAmmount as sqrroot of the break so that we can have o(sqr(n))
 
+    // jump until you find breaking step
+
+    // step one jump sqr root of break backwards and go to step 2 by checking every index
+
+    // if another break happens, return the index
+
+    // if not, return -1
+
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAmount;
+
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jmpAmount;
+
+    for (let j = 0; j <= jmpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
